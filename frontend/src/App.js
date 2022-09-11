5b66dc64a8c71cff76c87088de9eb3130e69c24d
john257398 <js4428900@gmail.com>
2022-09-11 14:47:45 -0700

    ecommerce

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
-import { useContext } from "react";
-import { ToastContainer } from "react-toastify";
+import { useContext, useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
-import { Badge, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import {
+  Badge,
+  Button,
+  Container,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
@@ -16,6 +23,8 @@ import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import OrderHistoryScreen from "./screens/OrderHistoryScreen";
 import ProfileScreen from "./screens/ProfileScreen";
+import { getError } from "./utils";
+import axios from "axios";
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -26,61 +35,114 @@ function App() {
     localStorage.removeItem("userInfo");
     localStorage.removeItem("shippingAddress");
     localStorage.removeItem("paymentMethod");
-    window.location.href='/signin'
+    window.location.href = "/signin";
   };
+
+  const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const { data } = await axios.get(`/api/products/categories`);
+        setCategories(data);
+      } catch (err) {
+        toast.error(getError(err));
+      }
+    };
+    fetchCategories();
+  }, []);
 
   return (
     <Router>
-      <div className='d-flex flex-column site-container'>
+      <div
+        className={
+          sidebarIsOpen
+            ? "d-flex flex-column site-container active-cont"
+            : "d-flex flex-column site-container"
+        }
+      >
         <ToastContainer position='bottom-center' limit={1} />
         <header>
           <Navbar bg='dark' variant='dark' expand='lg'>
             <Container>
+              <Button
+                variant='dark'
+                onClick={() => setSidebarIsOpen(!sidebarIsOpen)}
+              >
+                <i className='fas fa-bars'></i>
+              </Button>
               <Link to='/' className='code-doc'>
                 <Navbar.Brand>codeDoc</Navbar.Brand>
               </Link>
               <Navbar.Toggle aria-controls='basic-navbar-nav' />
-              <Navbar.Collapse id='basic-navbar-nav'>  
-
-              <Nav className='me-auto w-100 justify-content-end'>
-                <Link to='/cart' className='nav-link'>
-                  Cart
-                  {cart.cartItems.length > 0 && (
-                    <Badge pill bg='danger'>
-                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
-                    </Badge>
-                  )}
-                </Link>
-                {userInfo ? (
-                  <NavDropdown title={userInfo.name} id='basic-nav-dropdown'>
-                    <NavDropdown.Item>
-                      {" "}
-                      <Link to='/profile' className='navLink'>User Profile</Link>
-                    </NavDropdown.Item>
-                    <NavDropdown.Item>
-                      {" "}
-                      <Link to='/orderhistory' className='navLink'>Order History </Link>
-                    </NavDropdown.Item>
-
-                    <NavDropdown.Divider />
-                    <Link
-                      className='dropdown-item'
-                      to='#signout'
-                      onClick={signoutHandler}
-                    >
-                      Sign Out
-                    </Link>
-                  </NavDropdown>
-                ) : (
-                  <Link className='nav-link' to='/signin'>
-                    Sign In
+              <Navbar.Collapse id='basic-navbar-nav'>
+                <Nav className='me-auto w-100 justify-content-end'>
+                  <Link to='/cart' className='nav-link'>
+                    Cart
+                    {cart.cartItems.length > 0 && (
+                      <Badge pill bg='danger'>
+                        {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                      </Badge>
+                    )}
                   </Link>
-                )}
-              </Nav>
+                  {userInfo ? (
+                    <NavDropdown title={userInfo.name} id='basic-nav-dropdown'>
+                      <NavDropdown.Item>
+                        {" "}
+                        <Link to='/profile' className='navLink'>
+                          User Profile
+                        </Link>
+                      </NavDropdown.Item>
+                      <NavDropdown.Item>
+                        {" "}
+                        <Link to='/orderhistory' className='navLink'>
+                          Order History{" "}
+                        </Link>
+                      </NavDropdown.Item>
+
+                      <NavDropdown.Divider />
+                      <Link
+                        className='dropdown-item'
+                        to='#signout'
+                        onClick={signoutHandler}
+                      >
+                        Sign Out
+                      </Link>
+                    </NavDropdown>
+                  ) : (
+                    <Link className='nav-link' to='/signin'>
+                      Sign In
+                    </Link>
+                  )}
+                </Nav>
               </Navbar.Collapse>
             </Container>
           </Navbar>
         </header>
+        <div
+          className={
+            sidebarIsOpen
+              ? "active-nav side-navbar d-flex justify-content-between flex-wrap flex-column"
+              : "side-navbar d-flex justify-content-between flex-wrap flex-column"
+          }
+        >
+          <Nav className='flex-column text-white w-100 p-2'>
+            <Nav.Item>
+              <strong>Categories</strong>
+            </Nav.Item>
+            {categories.map((category) => (
+              <Nav.Item key={category}>
+                <Link
+                  to={`/search?category=${category}`}
+                  onClick={() => setSidebarIsOpen(false)}
+                >
+                  <Nav.Link>{category}</Nav.Link>
+                </Link>
+              </Nav.Item>
+            ))}
+          </Nav>
+        </div>
         <main>
           <Container className='mt-3'>
             <Switch>
